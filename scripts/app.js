a7e86f33ddfb2717ce5e163211a3458e0435a3ac
Alex Rogers <barryrogers@172-0-1-68.lightspeed.brhmal.sbcglobal.net>
2019-01-29 15:34:50 -0600

    changed array names into objects for later

--- scripts/app.js
+++ scripts/app.js
@@ -1,24 +1,40 @@
-const names = [
-  'Bob',
-  'Jane',
-  'Joseph',
-  'Miki',
-  'Selena',
-  'Katie',
-  'Trevor',
-  'Chris',
-  'Connor',
-  'Zachary',
-  'Stephanie',
-  'Ryan',
-  'Mara'
-];
+// const names = [
+//   'Bob',
+//   'Jane',
+//   'Joseph',
+//   'Miki',
+//   'Selena',
+//   'Katie',
+//   'Trevor',
+//   'Chris',
+//   'Connor',
+//   'Zachary',
+//   'Stephanie',
+//   'Ryan',
+//   'Mara'
+// ];
+
+const people = [
+  {name: 'Bob', level: 'C1', location: 'PHL'},
+  {name: 'Jane', level: 'C1', location: 'SEA'},
+  {name: 'Joseph', level: 'A2', location: 'PHL'},
+  {name: 'Miki', level: 'M3', location: 'ATL'},
+  {name: 'Selena', level: 'V1', location: 'CHI'},
+  {name: 'Katie', level: 'P2', location: 'HOU'},
+  {name: 'Trevor', level: 'C2', location: 'DAL'},
+  {name: 'Chris', level: 'A1', location: 'SFO'},
+  {name: 'Connor', level: 'M1', location: 'LAX'},
+  {name: 'Zachary', level: 'V4', location: 'CHI'},
+  {name: 'Stephanie', level: 'P1', location: 'SFO'},
+  {name: 'Ryan', level: 'C2', location: 'NYC'},
+  {name: 'Mara', level: 'M2', location: 'HOU'}
+]
 
 const namesList = document.querySelector('[data-names]');
 const duplicates = [];
 let arrayOfNames = [];
 for (let i = 0; i < 5; i++) {
-  let selectedName = names[Math.floor(Math.random()*names.length)];
+  let selectedName = people[Math.floor(Math.random()*people.length)].name;
   if (duplicates.indexOf(selectedName) !== -1) {
     i--;
     continue;
