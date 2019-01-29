e2cd5e066f50e8f492b77600fa099ab4da60bcac
Charmaine Estacio <charmaine.estacio@gmail.com>
2019-01-29 15:23:22 -0600

    add event listener to new fins button

--- scripts/app.js
+++ scripts/app.js
@@ -15,20 +15,31 @@ const names = [
 ];
 
 const namesList = document.querySelector('[data-names]');
-const duplicates = [];
-let arrayOfNames = [];
-for (let i = 0; i < 5; i++) {
-  let selectedName = names[Math.floor(Math.random()*names.length)];
-  if (duplicates.indexOf(selectedName) !== -1) {
-    i--;
-    continue;
+
+function selectNames() {
+  const duplicates = [];
+  for (let i = 0; i < 5; i++) {
+    let selectedName = names[Math.floor(Math.random() * names.length)];
+    if (duplicates.indexOf(selectedName) !== -1) {
+      i--;
+      continue;
+    }
+    duplicates.push(selectedName);
+    var node = document.createElement('LI'); // Create a <li> node
+    var textnode = document.createTextNode(selectedName); // Create a text node
+    node.appendChild(textnode);
+    namesList.appendChild(node);
   }
-  duplicates.push(selectedName);
-  var node = document.createElement("LI");                 // Create a <li> node
-  var textnode = document.createTextNode(selectedName);         // Create a text node
-  node.appendChild(textnode);
-  namesList.appendChild(node);
 }
+
+document
+  .getElementById('new-fins-button')
+  .addEventListener('click', function() {
+    namesList.innerHTML = '';
+    selectNames();
+  });
+
+selectNames();
 
 // node.appendChild(textnode);                              // Append the text to <li>
 // document.getElementById("myList").appendChild(node);
