34c144c630328f838030f571979b586cbbb221ad
Alex Rogers <barryrogers@BROGERS-MBP-ATL.local>
2019-01-31 14:05:03 -0600

    commented out non-usable pages

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -3,19 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
+// import { HomeComponent } from './home/home.component';
 import { InterestComponent } from './interest/interest.component';
-import { SignupComponent } from './signup/signup.component';
-import { UserProfileComponent } from './user-profile/user-profile.component';
+// import { SignupComponent } from './signup/signup.component';
+// import { UserProfileComponent } from './user-profile/user-profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
     InterestComponent,
-    SignupComponent,
-    UserProfileComponent
   ],
+  // HomeComponent,
+  // SignupComponent,
+  // UserProfileComponent
   imports: [
     BrowserModule,
     AppRoutingModule
