2793945c03065642ef4c708b026fa2af273b34c8
Alex Rogers <barryrogers@BROGERS-MBP-ATL.local>
2019-01-31 14:01:19 -0600

    fixed data binding for match component

--- my-app/src/app/interest/interest.component.ts
+++ my-app/src/app/interest/interest.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { PEOPLE } as people from '../list-people';
+
+const getRandomIndex = () => Math.floor(Math.random()*people.length);
+const duplicates = [];
 
 @Component({
   selector: 'app-interest',
@@ -9,7 +13,23 @@ export class InterestComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {
+  randomize() {
+    const randomFins = [];
+    for (let i = 0; i < 5; i++) {
+      let selectedFin = people[getRandomIndex()];
+      if (duplicates.includes(selectedFin.name)) {
+        i--;
+        continue;
+       }
+      duplicates.push(selectedFin.name);
+      randomFins.push(selectedFin);
+    }
+    return randomFins;
+  }
+
+  randomPeople = randomize();
+  onButtonClick() {
+    this.randomPeople = randomize();
   }
 
 }
